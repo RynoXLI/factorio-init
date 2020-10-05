8e6dad7cc1021181ea39208c030e27e385835342
Ryan Fogle <40377123+RynoXLI@users.noreply.github.com>
2020-10-04 22:50:19 -0500

    Delete check_staus.sh

--- check_staus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUT="$(sudo service factorio players-online)"
-
-if [ -z "$OUTPUT" ]; then
-       aws ec2 stop-instances --instance-ids i-0cd692cb7106be2b9
-else
-        echo $OUTPUT
-fi
